b1a254c7db059b8a235309bf8979c55a0dc5de1b
dianaaguilar22 <dianaisabel2001@outlook.es>
2023-03-08 19:43:16 -0600

    actividad 6

--- App.js
+++ App.js
@@ -1,6 +1,116 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+
+const App = () => {
+  const [materiaData, setMateriaData] = useState([
+    { key: 'Materia 1' },
+    { key: 'Materia 2' },
+    { key: 'Materia 3' },
+    { key: 'Materia 4' },
+    { key: 'Materia 5' },
+    { key: 'Materia 6' },
+    { key: 'Materia 7' },
+    { key: 'Materia 8' },
+    { key: 'Materia 9' },
+    { key: 'Materia 10' }
+  ]);
+
+  const [searchText, setSearchText] = useState('');
+
+  const searchMateria = () => {
+    const newData = materiaData.map((materia) => {
+      if (materia.key.toLowerCase().includes(searchText.toLowerCase())) {
+        return { ...materia, found: true };
+      } else {
+        return { ...materia, found: false };
+      }
+    });
+    setMateriaData(newData);
+  };
+
+  const Materia = ({ nombre, found }) => {
+    return (
+      <View style={[styles.item, found && styles.found]}>
+        <Text style={[styles.text, found && styles.foundText]}>{nombre}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Lista de Materias</Text>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar Materia"
+          onChangeText={(text) => setSearchText(text)}
+        />
+        <Button title="Buscar" onPress={searchMateria} />
+      </View>
+      <FlatList
+        style={styles.list}
+        data={materiaData}
+        renderItem={({ item }) => <Materia nombre={item.key} found={item.found} />}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  list: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    width: '100%',
+  },
+  item: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  text: {
+    fontSize: 16,
+  },
+  found: {
+    backgroundColor: '#f8d7da',
+  },
+  foundText: {
+    color: '#721c24',
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    marginVertical: 10,
+  },
+  searchInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 5,
+    marginRight: 10,
+    width: '70%',
+  },
+});
+
+export default App;
+
+
+
+
+/*import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
 
 export default function App() {
   const styles = StyleSheet.create({
@@ -60,7 +170,7 @@ export default function App() {
   const [materia3, setMateria3] = useState("");
   const [mostrar, setMostrar] = useState(false);
   
-  const inscribirse = () => {
+  const materias = () => {
     setMostrar(true);
   };
 
@@ -69,6 +179,7 @@ export default function App() {
       <View style={styles.header}>
         <Text style={styles.headerText}>Registro de Materias</Text>
       </View>
+
       <Text style={styles.indicador}>{"Primer materia"}</Text>
       <TextInput
         placeholder="Nombre de la materia 1"
@@ -92,7 +203,7 @@ export default function App() {
       />
       <Button
         onPress={() => {
-          inscribirse();
+          materias();
         }}
         disabled={mostrar}
         title={"Materias seleccionadas"}
@@ -112,4 +223,6 @@ export default function App() {
       <StatusBar style="auto" />
     </View>
   );
-}
+  
+  
+}*/
